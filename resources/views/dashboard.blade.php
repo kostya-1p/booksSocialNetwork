025224ec072a4fdb58519c35cb8a85944ff3a4d5
Pavel Kostitsyn <paulkostitsyn@gmail.com>
2022-08-03 13:51:57 +0400

    HTML for comment

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,45 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ 'Profile' }}
+            {{ 'User Profile' }}
         </h2>
+
+        <p> Name: {{ $user->name }} </p>
+        <p> Email: {{$user->email}} </p>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    User Profile <br><br>
+                    Comments:
+                </div>
+            </div>
+        </div>
+    </div>
 
-                    Name: {{ $user->name }} <br>
-                    Email: {{$user->email}}
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h1><b>Pavel Kostitsyn</b></h1>
+                    <p>Header</p>
+                    <p>My message</p>
+
+                    <p class="comment_date">12.07.2022</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <h1><b>Rita</b></h1>
+                    <p>Header</p>
+                    <p>My message</p>
+                    <p class="comment_date">12.07.2022</p>
                 </div>
             </div>
         </div>
